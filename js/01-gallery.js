b9d3e808b500a91bd21118238c1bf7a29612370b
WebGertsog <groxenvil@gmail.com>
2021-10-21 16:20:24 +0300

    Create
    Created gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarcap = createGallerysMarkap(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarcap);
+
+galleryContainer.addEventListener('click', onContainerClick);
+
+function createGallerysMarkap (galleryItems){
+  return galleryItems.map(({preview, original, description}) => {
+    return ` 
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+    </a>
+  </div>
+  `;
+  }).join(' ');
+}
+
+
+function onContainerClick(event) {
+   event.preventDefault();
+  if(event.target.nodeName !== 'IMG'){
+    return
+  }
+
+  const instance = basicLightbox.create(`<img width="1200" height="900" src="${event.target.dataset.source}" alt="${event.target.alt}">`)
+
+instance.show()
+
+
+
+window.addEventListener("keydown", EscClick);
+
+function EscClick(event) {
+    if(event.key === 'Escape') {
+      instance.close();
+      console.log(event.code);
+      window.removeEventListener("keydown", EscClick);
+    }
+  }
+}
